108518ef8de50dbe92beb7e8c9abb7b7104c2207
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-18 21:18:00 +0100

    game

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,63 @@
+let gagner = Math.floor(Math.random() * 100 +1);
+let number = document.getElementById('number');
+let random = document.getElementById('try');
+let last = document.getElementById('guess_last');
+let again = document.getElementById('again');
+let used = document.getElementById('used');
+let gg = document.getElementById('gg');
+let chance = 9;
+
+
+function myClick() {
+    if (chance > -1) {
+        if (parseInt(number.value) === gagner) {
+            gg.innerHTML = "C'est gagner !";
+            gg.style.textAlign = "center";
+            gg.style.fontSize = "2.5rem";
+            used.innerHTML += number.value + " ";
+        }
+        else if (parseInt(number.value) < gagner) {
+            gg.innerHTML = "Le nombre est trop petit !";
+            gg.style.textAlign = "center";
+            gg.style.fontSize = "2.5rem";
+            last.innerHTML = chance--;
+            used.innerHTML += number.value + " ";
+        }
+        else if (parseInt(number.value) > gagner) {
+            gg.innerHTML = "Le nombre est trop grand !";
+            gg.style.textAlign = "center";
+            gg.style.fontSize = "2.5rem";
+            last.innerHTML = chance--;
+            used.innerHTML += number.value + " ";
+        }
+        else {
+
+        }
+    }
+    else {
+        gg.innerHTML = "Et c'est perdu !";
+        gg.style.fontSize = "2rem";
+    }
+}
+
+function supp() {
+    number.value = null;
+    used.innerHTML = null;
+    last.innerHTML = '10';
+    gg.innerHTML = ' ';
+}
+
+
+random.addEventListener("click", myClick);
+
+again.addEventListener("click", supp);
+
+
+
+
+
+
+
+
+
 
